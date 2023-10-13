dc8f827dcd49c15c02dbe143825a8e63e3ff4b18
Nikita Shapovalov <nikita.shapovalov@exinity.com>
2023-10-13 15:18:00 +0400

    Support new versions

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 
 import PackageDescription
 
@@ -6,7 +6,8 @@ let package = Package(
     name: "Moya-ObjectMapper",
     products: [
         .library(name: "Moya-ObjectMapper", targets: ["Moya-ObjectMapper"]),
-        .library(name: "Moya-RxSwift-ObjectMapper", targets: ["Moya-RxSwift-ObjectMapper"])
+        .library(name: "Moya-RxSwift-ObjectMapper", targets: ["Moya-RxSwift-ObjectMapper"]),
+        .library(name: "Moya-ReactiveSwift-ObjectMapper", targets: ["Moya-ReactiveSwift-ObjectMapper"])
     ],
     dependencies: [
         .package(url: "https://github.com/Moya/Moya.git", .exact("14.0.0")),
@@ -28,8 +29,21 @@ let package = Package(
                 "Moya",
                 .product(name: "RxMoya", package: "Moya"),
                 "ObjectMapper",
+                "Moya-ObjectMapper",
                 "RxSwift"],
             path: "Source/RxSwift",
+            exclude: [
+                "Sample"]),
+        .target(
+            name: "Moya-ReactiveSwift-ObjectMapper",
+            dependencies: [
+                "Moya",
+                "ObjectMapper",
+                "Moya-ObjectMapper",
+                "ReactiveSwift",
+                .product(name: "ReactiveMoya", package: "Moya")
+            ],
+            path: "Source/ReactiveSwift",
             exclude: [
                 "Sample"])
     ]
