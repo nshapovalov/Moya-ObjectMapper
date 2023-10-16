06ee696ad9b71f0bdae0dd481dc8e3671e0cf740
Nikita Shapovalov <nikita.shapovalov@exinity.com>
2023-10-16 13:29:41 +0400

    Fix conflict with Moya

--- Package.swift
+++ Package.swift
@@ -6,14 +6,12 @@ let package = Package(
     name: "Moya-ObjectMapper",
     products: [
         .library(name: "Moya-ObjectMapper", targets: ["Moya-ObjectMapper"]),
-        .library(name: "Moya-RxSwift-ObjectMapper", targets: ["Moya-RxSwift-ObjectMapper"]),
-        .library(name: "Moya-ReactiveSwift-ObjectMapper", targets: ["Moya-ReactiveSwift-ObjectMapper"])
+        .library(name: "Moya-RxSwift-ObjectMapper", targets: ["Moya-RxSwift-ObjectMapper"])
     ],
     dependencies: [
         .package(url: "https://github.com/Moya/Moya.git", .exact("14.0.0")),
         .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", .upToNextMajor(from: "3.4.0")),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0")),
-        .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", .upToNextMajor(from: "6.0.0"))
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0"))
     ],
     targets: [
         .target(
@@ -33,18 +31,6 @@ let package = Package(
                 "Moya-ObjectMapper",
                 "RxSwift"],
             path: "Source/RxSwift",
-            exclude: [
-                "Sample"]),
-        .target(
-            name: "Moya-ReactiveSwift-ObjectMapper",
-            dependencies: [
-                "Moya",
-                "ObjectMapper",
-                "Moya-ObjectMapper",
-                "ReactiveSwift",
-                .product(name: "ReactiveMoya", package: "Moya")
-            ],
-            path: "Source/ReactiveSwift",
             exclude: [
                 "Sample"])
     ]
